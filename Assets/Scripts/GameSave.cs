a9a7ae061bd2c8fa479145be08c18603fc81eaf2
yorkalec <49527684+yorkalec@users.noreply.github.com>
2022-04-22 12:35:06 -0400

    Save manager, weapon select functionalities
    If the player has no save, they have to choose their weapon, otherwise they select their last saved level. Also note that in the level select menu, the on click events for the level select buttons has runtime as off because the save progress is still under development, there needs to be criteria in which designates whether the player has completed the level or not.

---
+++ Assets/Scripts/GameSave.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameSave : MonoBehaviour
+{
+    [SerializeField] public bool level1Complete;
+    [SerializeField] public bool level2Complete;
+    [SerializeField] public bool level3Complete;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (SaveManager.instance.hasLoaded)
+        {
+            level1Complete = SaveManager.instance.activeSave.level1Complete;
+            level2Complete = SaveManager.instance.activeSave.level2Complete;
+            level3Complete = SaveManager.instance.activeSave.level3Complete;
+        }   
+        else
+        {
+            SaveManager.instance.activeSave.level1Complete = level1Complete;
+            SaveManager.instance.activeSave.level2Complete = level2Complete;
+            SaveManager.instance.activeSave.level3Complete = level3Complete;
+        }
+    }
+
+    public void SaveProgress()
+    {
+        SaveManager.instance.activeSave.level1Complete = level1Complete;
+        SaveManager.instance.activeSave.level2Complete = level2Complete;
+        SaveManager.instance.activeSave.level3Complete = level3Complete;
+    }
+
+    public void level1()
+    {
+        level1Complete = true;
+    }
+
+    public void level2()
+    {
+        level2Complete = true;
+    }
+
+    public void level3()
+    {
+        level3Complete = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
